843fbd8fdaf1406d39bde82701e9b4196c0e47b8
Jess <jw1519@student.aru.ac.uk>
2025-06-05 10:57:08 +0100

    added game won and gameover panels

---
+++ Assets/scripts/Panels/GameOverPanel.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverPanel : BasePanel
+{
+    private void Start()
+    {
+        ClosePanel();
+    }
+    public void PlayerStatsDisplay()
+    {
+
+    }
+}
