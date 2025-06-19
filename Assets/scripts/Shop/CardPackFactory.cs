09c658df5ea4f0241d3d41fedf839ebb77477fa3
Jess <jw1519@student.aru.ac.uk>
2025-06-19 14:57:10 +0100

    organise scripts

---
+++ Assets/scripts/Shop/CardPackFactory.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Card
+{
+    public class CardPackFactory : MonoBehaviour
+    {
+        public GameObject cardPackGO;
+        public List<CardPack> cardPacks;
+
+        public void CreateCardPack()
+        {
+
+        }
+    }
+}
