955901ece9c9061ccc690a4d3d19c29412265810
Jess <jw1519@student.aru.ac.uk>
2025-06-22 14:04:18 +0100

    made restore health button

--- Assets/scripts/Panels/ShopPanel.cs
+++ Assets/scripts/Panels/ShopPanel.cs
@@ -1,17 +1,29 @@
+using Character;
 using TMPro;
+using UnityEngine;
 
 public class ShopPanel : BasePanel
 {
     public int AmountOfCards;
-
+    BaseCharacter character;
+    [Header("Reroll")]
     public int rerollCost;
     public TextMeshProUGUI rerollText;
 
+    [Header("Reastore Health")]
+    int healthRestorCost;
+    public TextMeshProUGUI healthRestoreText;
+
+    private void Awake()
+    {
+        character = FindAnyObjectByType<BaseCharacter>();
+    }
 
     private void OnEnable()
     {
-        ShopManager.instance.SetUpShop();
         rerollText.text = "Reroll " + rerollCost.ToString() + "g";
+        UpdateHealthRestoreUI();
+        ShopManager.instance.SetUpShop();
     }
     private void OnDisable()
     {
@@ -26,6 +38,30 @@ public class ShopPanel : BasePanel
             ShopManager.instance.ClearShop();
             ShopManager.instance.SetUpShop();
         }
+    }
+    public void RestoreHealth()
+    {
+        bool canBuy = ShopManager.instance.CanBuy(healthRestorCost);
+        if (canBuy)
+        {
+            character.health = character.maxHealth;
+            character.gameObject.GetComponent<SetCharacterUI>().UpdateHealthUI();
+        }
+    }
+    public void UpdateHealthRestoreUI()
+    {
+        healthRestorCost = character.maxHealth - character.health;
+        if (healthRestorCost > 0)
+        {
+            healthRestoreText.transform.parent.gameObject.SetActive(true);
+            healthRestoreText.text = "Restore Health " + healthRestorCost.ToString() + "g";
+        }
+        else
+        {
+            healthRestoreText.transform.parent.gameObject.SetActive(false);
+        }
+
+        
     }
 
 
