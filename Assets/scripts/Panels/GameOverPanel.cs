1396755119070738bd8a9bbe997d2766a6ef2ab8
Jess <jw1519@student.aru.ac.uk>
2025-07-20 22:44:14 +0100

    added high score save

--- Assets/scripts/Panels/GameOverPanel.cs
+++ Assets/scripts/Panels/GameOverPanel.cs
@@ -6,16 +6,34 @@ public class GameOverPanel : BasePanel
 {
     public int roundsWon;
     public int totalGoldEarned;
+
     public TextMeshProUGUI roundText;
     public TextMeshProUGUI goldEarnedText;
+    public TextMeshProUGUI highScoreText;
+
+    int highScore;
+    private void Start()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", highScore);
+    }
     public void PlayerStatsDisplay(BaseCharacter player)
     {
+        roundsWon = GameManager.instance.round--;
         roundText.text = "Rounds Won " + roundsWon.ToString();
         goldEarnedText.text = "Total Gold Earned " + player.totalGoldCollected.ToString();
+        UpdateHighScore();
+
+        highScoreText.text = "Most Rounds Won " + highScore.ToString();
     }
     public void NewRun()
     {
         GameManager.instance.NewRound();
         ClosePanel();
+    }
+
+    public void UpdateHighScore()
+    {
+        if (roundsWon > highScore)
+            highScore = roundsWon;
     }
 }
