39eb49d2acecc9c2a1aa05dcac0b54a47e7dca95
Jw1519 <jessica.woodward@aol.co.uk>
2026-06-02 13:54:29 +0100

    attempt to make map

--- Assets/scripts/Panels/MapPanel.cs
+++ Assets/scripts/Panels/MapPanel.cs
@@ -7,11 +7,60 @@ public class MapPanel : BasePanel
 
     public int mapSize = 5; // Size of the map (5x5)
     public float roomSpacing = 1.5f; // Spacing between rooms
+    public float roomSize = 1f; // Size of each room
 
     BaseRoom[,] grid;
+    public void Start()
+    {
+        grid = new BaseRoom[mapSize, mapSize];
+        CreateMap();
+    }
 
     public void CreateMap()
     {
+        for (int  i = 0; i < mapSize; i++)
+        {
+            for (int j = 0; j < mapSize; j++)
+            {
+                GameObject room = Instantiate(roomPrefab, new Vector3( i * roomSize - 5 * roomSpacing, j * roomSize - 5 * roomSpacing, 0), Quaternion.identity);
+                room.transform.localScale = Vector3.one * roomSize;
+                room.transform.SetParent(roomContainer, false);
 
+                grid[i, j] = room.GetComponent<BaseRoom>();
+
+                int roomTypeRoll = Random.Range(0, 100);
+
+                switch (roomTypeRoll)
+                {
+                    case int n when (n < 60):
+                        grid[i, j].InIt(i, j, RoomType.Normal);
+                        break;
+                    case int n when (n < 80):
+                        grid[i, j].InIt(i, j, RoomType.Boss);
+                        break;
+                    case int n when (n < 90):
+                        grid[i, j].InIt(i, j, RoomType.Shop);
+                        break;
+                    case int n when (n < 95):
+                        grid[i, j].InIt(i, j, RoomType.Treasure);
+                        break;
+                    default:
+                        grid[i, j].InIt(i, j, RoomType.Secret);
+                        break;
+                }
+
+                if (i == 0 && j == 0)
+                {
+                    grid[i, j].InIt(i, j, RoomType.Start);
+                    room.GetComponent<BaseRoom>().RevealRoom(); // Reveal the starting room
+                }
+                else if (i == mapSize - 1 && j == mapSize - 1)
+                {
+                    grid[i, j].InIt(i, j, RoomType.End);
+                }
+                room.GetComponent<BaseRoom>().SetUI();
+
+            }
+        }
     }
 }
--- Assets/scripts/Rooms/BaseRoom.cs
+++ Assets/scripts/Rooms/BaseRoom.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class BaseRoom : MonoBehaviour
 {
     public RoomType roomType;
     public Sprite roomImage;
+    Image image;
+    public TextMeshProUGUI text;
 
     public bool isCleared;
     public float roomChance; // Chance for this room to spawn, used in RoomManager when generating rooms
@@ -15,11 +18,62 @@ public class BaseRoom : MonoBehaviour
     {
         isCleared = false;
         isRevealed = false;
-        GetComponent<Image>().sprite = roomImage;
+        image = GetComponent<Image>();
+        text.text = roomType.ToString();
+        GetComponent<Button>().onClick.AddListener(EnterRoom);
+        if (roomType != RoomType.Start)
+            GetComponent<Button>().interactable = false; // Disable interaction until the room is revealed
+    }
+    public void SetUI()
+    {
+        text.text = roomType.ToString();
+        gameObject.name = roomType.ToString() + " Room (" + x + "," + y + ")";
+    }
+    public void InIt(int  x, int y, RoomType roomType)
+    {
+        this.x = x;
+        this.y = y;
+        this.roomType = roomType;
+    }
+    public void RevealRoom()
+    {
+        isRevealed = true;
+        image.sprite = roomImage;
+    }
+    public void ClearRoom()
+    {
+        isCleared = true;
+        GetComponent<Button>().interactable = false; // Disable interaction with the room
+        // reveal rooms around this one
+    }
+    public virtual void EnterRoom()
+    {
+        switch (roomType)
+        {
+            case RoomType.Normal:
+                Debug.Log("Entered Normal Room");
+                break;
+            case RoomType.Boss:
+                Debug.Log("Entered Boss Room");
+                break;
+            case RoomType.Shop:
+                Debug.Log("Entered Shop Room");
+                break;
+            case RoomType.Treasure:
+                Debug.Log("Entered Treasure Room");
+                break;
+            case RoomType.Secret:
+                Debug.Log("Entered Secret Room");
+                break;
+            case RoomType.End:
+                Debug.Log("Entered End Room");
+                break;
+        }
     }
 }
-public enum RoomType
+    public enum RoomType
 {
+    Start,
     Normal,
     Boss,
     Shop,
