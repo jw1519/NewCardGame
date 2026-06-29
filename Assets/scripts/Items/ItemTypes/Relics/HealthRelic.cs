6b43aca5c819caa6e690c964696589bf0a1287b6
Jess <jw1519@student.aru.ac.uk>
2026-06-29 14:30:03 +0100

    added health relic to add cards

--- Assets/scripts/Items/ItemTypes/Relics/HealthRelic.cs
+++ Assets/scripts/Items/ItemTypes/Relics/HealthRelic.cs
@@ -8,9 +8,17 @@ namespace Item
     {
         public BaseCard healthCard;
         public int healAmount;
+        public Abilty abilty;
         public override void Equip()
         {
-
+            switch (abilty)
+            {
+                case Abilty.AddCard:
+                    GameObject instance = CardFactory.instance.CreateCard(healthCard);
+                    AssetManager.Instance.GetAsset("CardManager").GetComponent<CardManager>().AddCard(instance);
+                    AssetManager.Instance.GetAsset("CardManager").GetComponent<CardManager>().AddCard(instance);
+                    break;
+            }
         }
         public enum Abilty
         {
